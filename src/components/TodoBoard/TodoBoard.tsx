b5f9b4ffe527e16fa83076791733113809d334d7
NetHunNull <nethunglitch@protonmail.com>
2026-05-15 18:50:15 +0200

    added useTodo hook

--- src/components/TodoBoard/TodoBoard.tsx
+++ src/components/TodoBoard/TodoBoard.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react"
 import {
   DndContext,
   closestCenter,
@@ -8,60 +7,19 @@ import {
 import AddTodo from "../AddTodo/AddTodo"
 import Column from "./TodoColumn"
 import Card from "./TodoCard"
-import type { Todo, ActiveId } from "../../types/todo"
+import { useTodos } from "../../hooks/useTodos"
 
 export default function Board() {
-  const [todos, setTodos] = useState<Todo[]>(() => {
-    const storedTodos = localStorage.getItem('tasks')
-    return storedTodos ? JSON.parse(storedTodos) : []
-  })
-
-  const [activeId, setActiveId] = useState<string | null>(null)
-  const activeTodo = todos.find(task => task.id === activeId)
-
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(todos)) 
-  }, [todos])
-
-  function addTodo(newTodo: Todo) {
-    setTodos(prev => [...prev, newTodo])
-  }
-
-  function delTodo(todoId: string) {
-    setTodos(prev => prev.filter(task => task.id !== todoId))
-  }
-
-  function moveTodo(todoId: string, newStage: Todo['stage']) {
-    setTodos(prev => prev.map(task => 
-      task.id === todoId ? {...task, stage: newStage} : task
-    ))
-  }
-
-  function onDragStart({ active }: any) {
-    setActiveId(String(active.id))
-  }
-
-  function onDragEnd({ active, over }: any) {
-    if (!over) {
-      setActiveId(null)
-      return
-    }
-
-    const stage = over.data?.current?.stage
-
-    if (!stage) {
-      setActiveId(null)
-      return
-    }
-
-    moveTodo(String(active.id), stage)
-
-    setActiveId(null)
-  }
-
-  function onDragCancel() {
-    setActiveId(null)
-  }
+  const {
+    todos, 
+    activeTodo, 
+    activeId, 
+    addTodo, 
+    delTodo, 
+    onDragStart, 
+    onDragEnd, 
+    onDragCancel
+  } = useTodos()
 
   return (
     <>
@@ -96,17 +54,17 @@ export default function Board() {
           />
         </main>
 
-      <DragOverlay>
-        {activeTodo ? (
-          <Card
-            id={activeTodo.id}
-            title={activeTodo.title}
-            description={activeTodo.description}
-            stage={activeTodo.stage}
-            onDelete={() => {}}
-          />
-        ) : null}
-      </DragOverlay>
+        <DragOverlay>
+          {activeTodo ? (
+            <Card
+              id={activeTodo.id}
+              title={activeTodo.title}
+              description={activeTodo.description}
+              stage={activeTodo.stage}
+              onDelete={() => {}}
+            />
+          ) : null}
+        </DragOverlay>
       </DndContext>
     </>
   )
